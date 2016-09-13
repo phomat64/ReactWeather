4c6a4b28c02aaa32f4265b0b55d977865c4a3563
Christopher Pham <christopherpham@Christophers-MacBook-Pro.local>
2016-09-12 20:09:08 -0500

    Update middleware for localhost

--- server.js
+++ server.js
@@ -6,10 +6,13 @@ var app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(function (req, res, next){
-  if (req.headers['x-forwarded-proto'] === 'http') {
-    next();
-  } else {
+  console.log('req.hostname: ' + req.hostname);
+  console.log('req.url: ' + req.url);
+  console.log('http://' + req.hostname + req.url);
+  if (req.headers['x-forwarded-proto'] === 'https') {
     res.redirect('http://' + req.hostname + req.url);
+  } else {
+    next();
   }
 })
 
